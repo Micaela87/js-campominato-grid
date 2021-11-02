1e952c4db52d7723549e25a72c5d353d56ddfcef
Micaela87 <84184729+Micaela87@users.noreply.github.com>
2021-11-02 16:36:44 +0100

    css and html issues fixed, block if else added in main.js

--- public/resources/js/main.js
+++ public/resources/js/main.js
@@ -5,18 +5,18 @@ const gameContainer = document.getElementById('game');
 const squareContainer = document.querySelector('.container-square');
 const levelButtons = document.querySelectorAll('button');
 
-const [easyLevel, mediumLevel, difficultLevel] = levelButtons;
-
-const easyLevelVal = Number(easyLevel.value),
-    mediumLevelVal = Number(mediumLevel.value),
-    difficultLevelVal = Number(difficultLevel.value);
-
 // generates squares according to the value of the button clicked
 // adds click event to each square generated
 const generateSquares = (number) => {
     for (let i = 0; i < number; i++) {
         let newDiv = document.createElement('div');
         newDiv.classList.add('square');
+        
+        if (number === 81) {
+            newDiv.classList.add('square-nine');
+        } else if (number === 49) {
+            newDiv.classList.add('square-seven');
+        }
 
         newDiv.addEventListener('click', 
             function() {
@@ -32,11 +32,9 @@ const generateSquares = (number) => {
 levelButtons.forEach(function(n) {
     n.addEventListener('click', 
         function() {
-            generateSquares(n.value);
+            generateSquares(Number(n.value));
             gameContainer.classList.remove('hidden');
             buttonContainer.classList.add('hidden');
         }
     )
-})
-
-console.log(typeof easyLevelVal);
+});
